9a59bc7631feac3d73baffa21a2f425e3600a350
Kotarou0 <kotarou@kotaroupc.localdomain>
2023-07-01 16:00:13 -0300

    Fixed some values.

--- 3rd-person camera/camera.gd
+++ 3rd-person camera/camera.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 const H_ACCELERATION = 3
 const V_ACCELERATION = 3
-const V_MIN = -25
+const V_MIN = -45
 const V_MAX = 80
 
 var direction = Vector2.ZERO
@@ -16,7 +16,7 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		direction.x += -event.relative.x * h_sensitivity
 		direction.y += event.relative.y * v_sensitivity
-		
+		print(direction.y)
 		direction.y = clamp(direction.y, V_MIN, V_MAX)
 
 func _physics_process(delta):
