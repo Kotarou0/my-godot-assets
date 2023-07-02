dcdf8757c772de434e089d5a149f8bc24db9ae3f
Kotarou0 <kotarou@kotaroupc.localdomain>
2023-07-01 21:13:49 -0300

    Added new asset. Improved camera system with more sense of speed.

--- 3rd-person character with camera/player.gd
+++ 3rd-person character with camera/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody3D
 
 const BASE_SPEED = 10.0
 const ACCELERATION = 5.0
+const DEFAULT_SPRING_LENGTH = 6.9
+const CAMERA_FOLLOW_SPEED = 4
+const CAMERA_FOLLOW_MULTIPLIER = 0.075
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var gravity_vector = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 var direction = Vector2.ZERO
@@ -11,8 +14,7 @@ func _input(event):
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = -Input.get_axis("ui_down", "ui_up")
 	direction = direction.normalized()
-	if camera:
-		direction = direction.rotated(-camera.global_rotation.y)
+	direction = direction.rotated(-camera.global_rotation.y)
 
 func _physics_process(delta):
 	if direction:
@@ -21,3 +23,7 @@ func _physics_process(delta):
 	velocity.z = lerp(velocity.z, direction.y * BASE_SPEED, ACCELERATION*delta)
 	velocity += gravity_vector*gravity*delta
 	move_and_slide()
+	
+	$Pivot/h/v.spring_length = lerp($Pivot/h/v.spring_length, 
+		DEFAULT_SPRING_LENGTH*(1+velocity.length()*CAMERA_FOLLOW_MULTIPLIER), 
+		CAMERA_FOLLOW_SPEED * delta)
